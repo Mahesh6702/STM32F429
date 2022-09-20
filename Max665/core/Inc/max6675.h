9af4040019f0b1f4e1fef8d3ec263b0aeeb90cbe
Mahesh Eega <79939325+Mahesh6702@users.noreply.github.com>
2022-09-20 16:12:34 +0200

    Add files via upload

---
+++ Max665/core/Inc/max6675.h
@@ -0,0 +1,13 @@
+/*
+ * max6675.h
+ *
+ *  Created on: Sep 10, 2022
+ *      Author: eegam
+ */
+
+#ifndef INC_MAX6675_H_
+#define INC_MAX6675_H_
+#include "main.h"
+
+float Max6675_temparature(void);
+#endif
